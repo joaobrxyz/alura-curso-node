6b3cd99fc00f1ac1c33393224c70d50751b78e3a
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-12-11 21:05:17 -0300

    Criei registros com POST

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json()); // Middleware
 
 const livros = [
     {
@@ -13,12 +14,17 @@ const livros = [
     }
 ];
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
 
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso!");
 });
 
 export default app;
