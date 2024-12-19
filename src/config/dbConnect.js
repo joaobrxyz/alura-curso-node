449983d06dfc12994284c1ff159802e71227041f
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-12-18 21:18:22 -0300

    Acessando a coleção livros

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose  from "mongoose";
 
 async function conectaNaDatabase() {
-    mongoose.connect("mongodb+srv://admin:admin123@cluster0.qeilg.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
 
     return mongoose.connection;
 };
