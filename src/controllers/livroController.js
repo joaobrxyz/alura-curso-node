22fd7f5eeae1e2a8dcf3c8a8958df3e30df64311
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-01-06 21:16:02 -0300

    Controller DELETE

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -4,7 +4,7 @@ class LivroController {
     static async listarLivros (req, res) {
         try {
             const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
+            res.status(200).json(listaLivros);
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} falha na requisição` });
         }
@@ -15,7 +15,7 @@ class LivroController {
         try {
             const id = req.params.id;
             const livroEncontrado = await livro.findById(id);
-        res.status(200).json(livroEncontrado);
+            res.status(200).json(livroEncontrado);
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} falha na requisição do livro` });
         }
@@ -34,9 +34,19 @@ class LivroController {
         try {
             const id = req.params.id;
             await livro.findByIdAndUpdate(id, req.body);
-        res.status(200).json({ message: "Livro atualizado"});
+            res.status(200).json({ message: "Livro atualizado"});
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} falha na atualização` });
+        }
+    };
+
+    static async excluirLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({ message: "livro excluído com sucesso!" });
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} falha na exclusão` });
         }
     };
 };
