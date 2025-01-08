927d45fc9ad83d127e008929bfa44d50e2329a01
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-01-07 21:04:08 -0300

    Unindo livros e autores

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,4 +1,5 @@
 import livro from "../models/Livro.js";
+import { autor } from "../models/Autor.js";
 
 class LivroController {
     static async listarLivros (req, res) {
@@ -22,8 +23,12 @@ class LivroController {
     };
 
     static async cadastrarLivro (req, res) {
+        const novoLivro = req.body;
+
         try {
-            const novoLivro = await livro.create(req.body);
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc }};
+            const livroCriado = await livro.create(livroCompleto);
             res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
