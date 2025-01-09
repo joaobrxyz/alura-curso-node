6e351a78365a65b4b9dd7e0bde55fc54ebffed06
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-01-08 21:30:54 -0300

    Att para o novo curso
    Att necessárias para o curso Node.js: lidando com buscas, filtros, paginação e erros em uma API.  Que é uma continuação.

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,9 +1,7 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose"
 
-async function conectaNaDatabase() {
-    mongoose.connect(process.env.DB_CONNECTION_STRING);
+mongoose.connect(process.env.DB_CONNECTION_STRING);
 
-    return mongoose.connection;
-};
+let db = mongoose.connection;
 
-export default conectaNaDatabase;
+export default db;
